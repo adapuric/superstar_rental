765f4f657a66c3f3d85aa9998d4fd2e6a789dbfc
Ada Puric <puric.ada1@gmail.com>
2021-11-16 17:26:07 +0000

    booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+  before_action :set_superstar, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @superstar = Superstar.find(params[:superstar_id])
+    @booking = Booking.new(booking_params)
+    @booking.superstar = @superstar
+    if @booking.save
+      redirect_to superstar_path(@superstar)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:event_date)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_superstar
+    @superstar = Superstar.find(params[:superstar_id])
+  end
 end
