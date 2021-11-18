77bc70394c9e73e85dd5f0178c1f54c1967ee1cf
Ada Puric <puric.ada1@gmail.com>
2021-11-18 15:28:21 +0000

    some changes to filter

--- app/controllers/superstars_controller.rb
+++ app/controllers/superstars_controller.rb
@@ -5,7 +5,7 @@ class SuperstarsController < ApplicationController
     else
       @superstars = Superstar.all
     end
-    if params[:button] && params[:query] != "" && Superstar.all.event_types.include?(params[:query])
+    if params[:button] && params[:query] != "" && Superstar.all.event_type.include?(params[:query])
       @superstars = Superstar.all.select { |superstar| superstar.event_type.downcase.include?(params[:query].downcase) }
     elsif params[:button] && params[:query] != ""
       @superstars = Superstar.all.select { |superstar| superstar.name.downcase.include?(params[:query].downcase) }
