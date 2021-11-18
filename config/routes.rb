9eca54e17f168a4b8f3c080ce95a2a344638bab3
Alexandre Magalhaes <alexandremagalhaes99@gmail.com>
2021-11-18 13:06:16 -0300

    added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "dashboard", to: "pages#dashboard"
+
+  resources :users, only: [:show]
+
   resources :superstars do
   resources :bookings, only: [:new, :create]
   resources :reviews, only: [:create]
